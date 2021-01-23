f5c2be4b2c448b21b1f1682f602be6b416e4eb62
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 01:37:57 +0600

    Create 10783 Solution.cpp

---
+++ UVA/10783 Solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, a, b;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++)
+    {
+        int sum=0;
+        scanf("%d%d", &a, &b);
+        for(int j=a; j<=b; j++)
+        {
+            if(j%2!=0)
+            {
+                sum+=j;
+            }
+        }
+        printf("Case %d: %d\n", i, sum);
+    }
+}
