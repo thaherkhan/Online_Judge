1eec51a1f8a20cbc9832d235db0e41ef282079b8
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 00:54:33 +0600

    Create 12709 Problem.cpp

---
+++ UVA/12709 Problem.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+double const g=9.8;
+
+int main()
+{
+    ll t,cas=1;
+    while(cin>>t)
+    {
+        ll L,W,H,vlm=0,mxH=0,mxV=0;
+        if(t==0)
+            return 0;
+        while(t--)
+        {
+            cin>>L>>W>>H;
+            if(H>mxH)
+            {
+                vlm=L*W*H;
+                mxH=H;
+            }
+            if(mxH==H)
+                vlm=max(vlm, L*W*H);
+        }
+        cout<<vlm<<endl;
+    }
+
+}
