9192810d8b131dbb0a9d558126d73266b77200a0
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 02:43:46 +0600

    Create 1294 Solution.cpp

---
+++ LightOJ/1294 Solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const double pi = 3.1415926535898;
+
+int main()
+{
+    ll t,cas=1;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m,sum=0;
+        cin>>n>>m;
+        cout<<"Case "<<cas<<": "<<(n*m)/2<<endl;
+        cas++;
+    }
+}
