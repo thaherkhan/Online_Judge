352a79804c26eb1e27b50dc33ab8646d60a6344e
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 01:02:21 +0600

    Create 10055 Solution.cpp

---
+++ UVA/10055 Solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dl double
+
+int main()
+{
+    ll n,m;
+    while(scanf("%lld%lld",&n,&m)!=EOF)
+    {
+        printf("%lld\n",abs(n-m));
+    }
+}
