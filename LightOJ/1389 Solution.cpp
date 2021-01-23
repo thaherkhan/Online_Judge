9e313164bb31f93fbb275a74f81daffc3b31820b
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 03:18:01 +0600

    Create 1389 Solution.cpp

---
+++ LightOJ/1389 Solution.cpp
@@ -0,0 +1,25 @@
+include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define lp1 for(int k=0; k<s.length(); k++)
+
+int main()
+{
+    int t,j=1;
+    cin>>t;
+    while(t--){
+        int n,coun=0;
+        string s;
+        cin>>n;
+        cin>>s;
+        lp1{
+            if(s[k]=='#')
+                continue;
+
+            coun++;
+            k+=2;
+        }
+        cout<<"Case "<<j<<": "<<coun<<endl;j++;
+    }
+}
