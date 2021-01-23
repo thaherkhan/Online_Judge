8cb78a1cf5dc5f2ff03cde4ff1409858bbb0637a
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-01-24 01:25:13 +0600

    Create 136 Solution.cpp

---
+++ UVA/136 Solution.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define dl double
+#define lp for(ll i=0; i<n; i++)
+#define lp2 for(ll j=0; j<m; j++)
+#define lps for(ll k=0; k<len; k++)
+
+int main(){
+
+    cout<<"The 1500'th ugly number is 859963392."<<endl;
+}
